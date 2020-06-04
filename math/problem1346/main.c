e09c27168c8c7d83f661767da5a936cae80ddfbd
beller0ph0nt <beller0ph0nt@users.noreply.github.com>
2020-06-04 19:21:50 +0700

    Update main.c

--- math/problem1346/main.c
+++ math/problem1346/main.c
@@ -1,14 +1,3 @@
-/**
-    The following math problem illustrates the essence of hacking:
-
-    Use each of the numbers 1, 3, 4, and 6 exactly once with any
-  of the four basic math operations (addition, subtraction,
-  multiplication, and division) to total 24. Each number must be
-  used once and only once, and you may define the order of
-  operations.For example, 3 * (4 + 6) + 1 = 31 is valid, however
-  incorrect, since it doesn’t total 24.
-**/
-
 #include <stdio.h>
 
 #define DIGIT_LEN       4
